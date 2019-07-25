60e43683cc810f3ad5955b520aee3b5e074bbf2f
Arif BAĞLAR <arifbaglar@hotmail.com>
2019-07-25 14:33:12 +0000

    Automatically backed up by Learn

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -8,7 +8,11 @@ export default class ColorBox extends Component {
  
         if (this.props.opacity>= 0.2) {
             return (
+<<<<<<< HEAD
                 <div className="color-box" style={{opacity:1}}>
+=======
+                <div className="color-box" style={{opacity: 2}}>
+>>>>>>> 13cfe2a902ff63288fb4f1aed552a7f8e8904835
                     <ColorBox opacity={newValue} />
                 </div>
             );
@@ -22,7 +26,15 @@ export default class ColorBox extends Component {
     
   
     
+<<<<<<< HEAD
     
+=======
+    return (
+      <div className="color-box" style={{opacity: 2}}>
+        {this.props.opacity}
+      </div>
+    )
+>>>>>>> 13cfe2a902ff63288fb4f1aed552a7f8e8904835
   }
   
 }
